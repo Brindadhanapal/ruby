68c1f224dcc3fbdac3fe5cf468487ef3569bb83d
Brindadhanapal <103595634+Brindadhanapal@users.noreply.github.com>
2022-04-19 02:16:13 -0400

    Add files via upload
    Adding ex9.rb

---
+++ ex9.rb
@@ -0,0 +1,15 @@
+#Here's some new strange stuff,
+
+days = "Mon Tue Wed Thurs Fri Sat Sun"
+month = "\nJan\nFeb\nMar\nApr\nMay\nJun\njul\nAug\nSep\nOct\nNov\nDec"
+
+puts "Here are the days #{days}."
+puts "Here are the months#{month}."
+
+puts %q{
+  There's something going on here.
+  with this weird quote
+  we'll be able to type as much as we like.
+  Even 4 lines if we want, or 5, or 6.
+
+}
